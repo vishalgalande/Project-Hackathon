a1f83c49aed911115e79131b0e276ba1506622d9
Prajnadeep Sarma <prajnadeepsarma@gmail.com>
2026-01-14 15:02:09 +0530

    flutter
    downloaded some shit

---
+++ frontend/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Jaideep\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Jaideep\\OneDrive\\Documents\\GitHub\\Project-Hackathon\\frontend" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Jaideep\\flutter"
+  "PROJECT_DIR=C:\\Users\\Jaideep\\OneDrive\\Documents\\GitHub\\Project-Hackathon\\frontend"
+  "FLUTTER_ROOT=C:\\Users\\Jaideep\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Jaideep\\OneDrive\\Documents\\GitHub\\Project-Hackathon\\frontend\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Jaideep\\OneDrive\\Documents\\GitHub\\Project-Hackathon\\frontend"
+  "FLUTTER_TARGET=C:\\Users\\Jaideep\\OneDrive\\Documents\\GitHub\\Project-Hackathon\\frontend\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzguNg==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049OGI4NzI4Njg0OQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NzhmYzMwMTJlNA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC43"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Jaideep\\OneDrive\\Documents\\GitHub\\Project-Hackathon\\frontend\\.dart_tool\\package_config.json"
+)
